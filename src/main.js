f3e59a6b73921a1359122aa4653fcf260e0fff56
Parker <parker@Parkers-MacBook-Pro-2.local>
2017-03-12 17:34:30 -0700

    add loops and stages. Practicin' mah shortcuts

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Loop, Stage } from 'react-game-kit/native';
+
+export default class Game extends Component {
+
+  render() {
+    return (
+      <Loop>
+        <Stage>
+
+        </Stage>
+      </Loop>
+    );
+  }
+
+}
